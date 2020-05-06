7ee679b2801d61a8ec9d6e051b79732dcf8481fc
tnyima <60240526+tnyima@users.noreply.github.com>
2020-05-05 21:59:09 -0400

    Update ManagePersons.java

--- src/ManagePersons.java
+++ src/ManagePersons.java
@@ -1,26 +1,28 @@
 import comp127graphics.CanvasWindow;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class ManagePersons {
 
+/***
+ * This class manages the people. It generates people for our simulation and checks the health status and whether
+ * the people in the simulation had collided with an infected person.
+ */
+public class ManagePersons{
     private CanvasWindow canvas;
     private List<Person> allPersons = new ArrayList<>();
-    private double transmissionRate = 0.5;
     private Random ran = new Random();
-
-
-
-
     public ManagePersons(CanvasWindow canvas){
         this.canvas = canvas;
 
     }
-    public List<Person> generate(int numPeople){
+
+    /** Takes in a number and generates the sample population based of that number*/
+    public List<Person> generate(int numPeople, int time, int infectiousPeriod){
         for (int i = 0; i < numPeople; i++ ){
             Person person = new Person(canvas);
+            person.setRecoveredTime(time);
+            person.setInfectiousPeriod(infectiousPeriod);
             allPersons.add(person);
             canvas.add(person);
         }
@@ -30,28 +32,27 @@ public class ManagePersons {
 
     /** Checks if a persons collided with a person who is infected and changes
      *  their infected state and color if they do */
-    public void checkInfectedCollision(Person person){
+    public void checkInfectedCollision(Person person, double transmissionRate){
         if (!person.infected && !person.recovered){
             for (Person secondPerson  : allPersons){
                 if (person.detectCollision() == secondPerson){
                     if (secondPerson.infected){
                         double chance = ran.nextDouble();
-                        System.out.println(secondPerson.infectiousPeriod);
-                        if (secondPerson.infectiousPeriod > 0 && chance < 0.1 )
+                        if (secondPerson.infectiousPeriod > 0 && chance < transmissionRate)
                             person.makeInfected();
                     }
                 }
 
             }
         }
-
     }
+
     /** Updates the recovery time of a person, and change their status to recovered when their recovery
      * time reaches zero
      * @return*/
     public void checkHealthStatus(Person person) {
         if (person.infected) {
-            int like = person.recoveryTime--;
+            person.recoveryTime--;
             if (!(person.infectiousPeriod <= 0)) {
                 person.infectiousPeriod--;
             }
@@ -59,40 +60,8 @@ public class ManagePersons {
         if (person.recoveryTime == 0) {
             person.makeRecovered();
         }
-
     }
 
-//    /***
-//     * This generates the sample population.
-//     * @param numPeople
-//     * @return
-//     */
-//
-//    public List<Person> generate(int numPeople){
-//        for (int i = 0; i < numPeople; i++ ){
-//            Person person = new Person(canvas);
-//            allPersons.add(person);
-//            canvas.add(person);
-//        }
-//        allPersons.get(0).makeInfected();
-//
-//        return allPersons;
-//    }
-//
-//    /** Checks if a persons collided with a person who is infected and changes
-//     *  their infected state and color if they do */
-//    public void checkInfectedCollision(Person person){
-//        if (person.getInfected()){
-//            for (Person secondPerson : allPersons){
-//                if (secondPerson.detectCollision() == person){
-//                    secondPerson.makeInfected();
-//                    System.out.println("Hello");
-//                }
-//            }
-//
-//        }
-//
-//    }
 }
 
 
