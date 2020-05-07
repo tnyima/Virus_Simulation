2304b3e856f4712d232029815ca808d7f05aee52
AhmedMac <60229280+AhmedMac@users.noreply.github.com>
2020-05-07 12:38:30 -0500

    Update Simulation.java

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,31 +1,46 @@
 import comp127graphics.CanvasWindow;
 import comp127graphics.Point;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+// You need to add the org.knowm.xchart:xchart:3.6.1 library using maven so that these imports work.
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 
-import org.jfree.chart.*; // you have to add Jfree to your library to run have the graph method work
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.ui.ApplicationFrame;
 
 
 /**
  * This class runs the simulation.
  */
 public class Simulation {
+
     private CanvasWindow canvas;
     private final static int WINDOW_HEIGHT = 800;
     private final static int WINDOW_WIDTH = 800;
+
     private ManagePersons managePersons;
     private List<Person> allPersons;
     private double transmissionRate;
-    private int recoveryTime;
-    private int infectiousPeriod;
-    private long elapsedTime = 0;
-    private DefaultCategoryDataset dataset;
-    private long startTime, endTime;
 
+    private long elapsedTime = 0;
+
+
+    SwingWrapper<XYChart> sw;
+    private List<Number> timeList = new ArrayList<>(),
+            infectedNumList = new ArrayList<>(),
+            recoveredNumList = new ArrayList<>();
+    List<Person> allInfected;
+    List<Person> allRecovered;
+
+    private XYChart lineChart;
+
+
+    /** Initializes canvas, uses scanner to ask user for transmission rate, infectious period, recovery time
+     * and the sample size, which is used to generate person objects.  */
     public Simulation() {
         canvas = new CanvasWindow("Virus Simulation", WINDOW_WIDTH, WINDOW_HEIGHT);
         managePersons = new ManagePersons(canvas);
@@ -37,25 +52,26 @@ public class Simulation {
         int sampleSize = numPopulation.nextInt();
 
         Scanner transRate = new Scanner(System.in);
-        System.out.println("Select an infectious rate within the range of 0 to 1: ");
-        transmissionRate = transRate.nextDouble();
+
+        // Checks if user input is within the accepted range
+        do{
+            System.out.println("Enter a transmission rate between 0 and 1");
+            transmissionRate = transRate.nextDouble();
+        } while (transmissionRate > 1);
 
         Scanner recovered = new Scanner(System.in);
         System.out.println("Select the recovery time: ");
-        recoveryTime = recovered.nextInt();
+        int recoveryTime = recovered.nextInt();
 
         Scanner infectPeriod = new Scanner(System.in);
         System.out.println("Select the infectious Period of the Virus: ");
-        infectiousPeriod = infectPeriod.nextInt();
+        int infectiousPeriod = infectPeriod.nextInt();
 
+        // Generates person objects and stores them in the allPersons list.
         allPersons = managePersons.generate(sampleSize, recoveryTime, infectiousPeriod);
     }
 
-    /**
-     * This is the main method that runs the simulation.
-     *
-     * @param args
-     */
+    /** This is the main method that runs the simulation. */
     public static void main(String[] args) {
         Simulation simulation = new Simulation();
         simulation.run();
@@ -63,48 +79,71 @@ public class Simulation {
 
 
     /**
-     * This method runs the program.
+     * This method runs the program. It has a while loop that runs as long as the end condition is not met.
+     * Keeps track of time within program using nanoTime.
      */
     private void run() {
-        startTime = System.nanoTime() / 1000000000;
+        setUpLists();
+        createGraph();
+        long startTime = System.nanoTime() / 1000000000;
         while (!endCondition()) {
-            move();
-            endTime = System.nanoTime() / 1000000000;
+            updatePerson();
+            long endTime = System.nanoTime() / 1000000000;
             elapsedTime = (endTime - startTime);
+            setUpLists();
+            updateGraph();
         }
     }
 
-    public void addToDataSet(int numPersons, long time, String stringName) {
-        dataset = new DefaultCategoryDataset();
-        dataset.addValue(numPersons, stringName, String.valueOf(time));
+    /** It initializes the line chart and displays it on screen */
+    public void createGraph() {
+        lineChart = QuickChart.getChart("Infected people over time", "Time", "People",
+                "infected",timeList, infectedNumList);
+        lineChart.addSeries("recovered", timeList, recoveredNumList);
+        sw = new SwingWrapper<XYChart>(lineChart);
+        sw.displayChart();
     }
 
-    public void graph() {
-        JFreeChart lineChart = ChartFactory.createLineChart("Virus Simulation",
-                "Time", "Number of People", dataset, PlotOrientation.VERTICAL,
-                false, false, false);
-        ChartPanel chartPanel = new ChartPanel(lineChart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
-        ApplicationFrame frame = new ApplicationFrame("Virus Simulation");
-        frame.setContentPane(chartPanel);
-        frame.pack();
-        frame.setVisible(true);
+    /** It creates the lists necessary to create the graphs in the createGraph() method.*/
+    public void setUpLists(){
+        allInfected = allPersons.stream()
+                .filter(person -> person.infected)
+                .collect(Collectors.toList());
+
+        allRecovered = allPersons.stream()
+                .filter((person -> person.recovered))
+                .collect(Collectors.toList());
+
+        recoveredNumList.add(allRecovered.size());
+        infectedNumList.add(allInfected.size());
+        timeList.add(elapsedTime);
     }
 
-    public void move() {
+    /** Updates the data of the line chart and updates the line chart so that the changes are displayed */
+    public void updateGraph(){
+        lineChart.updateXYSeries("infected",timeList,infectedNumList, null);
+        lineChart.updateXYSeries("recovered", timeList, recoveredNumList, null);
+        sw.repaintChart();
+    }
+
+    /** Updates the position of each person object randomly so that they move. Also, checks for
+     * collision and health status */
+    public void updatePerson() {
         for (Person person : allPersons) {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
             person.moveRandomly(canvasCenter);
-            managePersons.checkHealthStatus(person, elapsedTime, endTime);
+            managePersons.checkHealthStatus(person, elapsedTime);
             managePersons.checkInfectedCollision(person, transmissionRate, elapsedTime);
         }
         canvas.draw();
         canvas.pause(10);
     }
 
+    /** Checks if there are no more infected persons and returns result in boolean*/
     public boolean endCondition() {
         return allPersons.stream().noneMatch(person -> person.infected);
     }
 }
+
 
 
